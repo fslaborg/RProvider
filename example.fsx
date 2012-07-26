b353b2a55ef26f74af8959dfb425ffc2b6f87408
hmansell <hmansell@bluemountaincapital.com>
2012-07-26 18:03:15 -0400

    Added example script

---
+++ example.fsx
@@ -0,0 +1,46 @@
+﻿#r @"References/RDotNet.dll"
+#r @"bin/Debug/RProvider.dll"
+ 
+open RDotNet
+open RProvider
+open RProvider.``base``
+open RProvider.graphics
+open RProvider.stats
+open RProvider.tseries
+open RProvider.zoo
+ 
+open System
+open System.Net
+
+// URL of a service that generates price data
+let url = "http://ichart.finance.yahoo.com/table.csv?s="
+ 
+/// Returns prices (as tuple) of a given stock for a
+/// specified number of days (starting from the most recent)
+let getStockPrices stock count =
+    // Download the data and split it into lines
+    let wc = new WebClient()
+    let data = wc.DownloadString(url + stock)
+    let dataLines = data.Split([| '\n' |], StringSplitOptions.RemoveEmptyEntries)
+ 
+    // Parse lines of the CSV file and take specified
+    // number of days using in the oldest to newest order
+    seq { for line in dataLines |> Seq.skip 1 do
+              let infos = line.Split(',')
+              yield float infos.[4] }
+    |> Seq.take count |> Array.ofSeq |> Array.rev
+
+let msft = getStockPrices "MSFT" 255 |> R.log |> R.diff 
+ 
+//compute the autocorrelation of msft stock prices
+let a = R.acf(msft)
+
+//lets see if the msft returns are stationary/non-unit root
+let adf = R.adf_test(msft) 
+
+//lets look at some pair plots
+let tickers = [ "MSFT"; "AAPL"; "X"; "VXX"; "SPX"; "GLD" ]
+let data = [ for t in tickers -> t, getStockPrices t 255 |> R.log |> R.diff ]
+let df = R.data_frame(namedParams data)
+R.pairs(df)
+
