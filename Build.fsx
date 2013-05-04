a2cd3f523a3884a80a8b3266dcf02d2b35837d40
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-05-04 13:01:14 -0700

    Added Build base script and NuGet.exe
    Base setup for FAKE: added NuGet.exe in tools folder, and build.bat /
    build.fsx.

---
+++ Build.fsx
@@ -0,0 +1,11 @@
+﻿// include Fake lib
+#r @"tools\FAKE\tools\FakeLib.dll"
+open Fake 
+ 
+// Default target
+Target "Default" (fun _ ->
+    trace "Hello World from FAKE"
+)
+ 
+// start build
+Run "Default"
