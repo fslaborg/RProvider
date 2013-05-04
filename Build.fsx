6c0c56776c1cd524f4325d8ca22d9b6926773f0e
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-05-04 13:24:59 -0700

    Clean & Build added to build script.

--- Build.fsx
+++ Build.fsx
@@ -1,11 +1,29 @@
 ﻿// include Fake lib
 #r @"tools\FAKE\tools\FakeLib.dll"
 open Fake 
- 
+
+// Properties
+let buildDir = @".\build\"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "BuildApp" (fun _ ->
+    !! @"rprovider.fsproj"
+      |> MSBuildRelease buildDir "Build"
+      |> Log "AppBuild-Output: "
+)
 // Default target
 Target "Default" (fun _ ->
-    trace "Hello World from FAKE"
+    trace "Building R Provider"
 )
- 
+
+// Dependencies
+"Clean"
+  ==> "BuildApp"
+  ==> "Default"
+
 // start build
 Run "Default"
