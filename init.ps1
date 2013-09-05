263cf828c7a4ee8a99b4befdce80b0440c250655
Sergey Tihon <sergey.tihon@gmail.com>
2013-09-05 09:38:16 +0300

    Fixed bugs in init.ps1

--- init.ps1
+++ init.ps1
@@ -1,9 +1,9 @@
 param($installPath, $toolsPath, $package)
 
 $destPath = $installPath + "\lib\"
-Foreach($p in $package.Dependencies)
+Foreach($p in $package.DependencySets.Dependencies)
 {
-    $path = $installPath + "\..\" + (p.Id) + "." + (p.Version) + "\lib\" 
+    $path = $installPath + "\..\" + ($p.Id) + "." + ($p.VersionSpec) + "\lib\" 
     $files = Get-ChildItem -Recurse $path | where {$_.PSIsContainer -eq $False}
     Foreach ($file in $files)   
     {  
