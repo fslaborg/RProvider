fb14af887ebb0f5fb50730056b3f4983afd7396e
J-M <jean-michel.perraud@csiro.au>
2014-02-26 14:51:46 +1100

    Commit updates done to investigate https://rdotnet.codeplex.com/workitem/89 (resolved as not an issue)

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,4 @@
 * 1.0.3 - Fixed NuGet package
 * 1.0.4 - Improve stability (refactor initialization), add documentation
 * 1.0.5 - Update load script in the NuGet package, improve cross-platform support
+* 1.0.6 - Update to use R.NET 1.5.6
--- src/Common/AssemblyInfo.fs
+++ src/Common/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("BlueMountain Capital")>]
 [<assembly: AssemblyProductAttribute("RProvider")>]
 [<assembly: AssemblyDescriptionAttribute("An F# Type Provider providing strongly typed access to the R statistical package.")>]
-[<assembly: AssemblyVersionAttribute("1.0.5")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.5")>]
+[<assembly: AssemblyVersionAttribute("1.0.6")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.6")>]
 
 ()
