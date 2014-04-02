edf75f6226b1c1e057c4c068a6cdd0bd0a47364b
David Charboneau <david@adadabase.com>
2014-04-02 13:51:58 -0400

    Add RInterop loading in RProvider.fsx
    It will not run without it.

--- src/RProvider/RProvider.fsx
+++ src/RProvider/RProvider.fsx
@@ -4,13 +4,19 @@
 #I "../bin"
 #I "bin"
 #I "lib"
+#I "packages/RInterop.1.0.5/lib"
+#I "../packages/RInterop.1.0.5/lib"
+#I "../../packages/RInterop.1.0.5/lib"
+#I "../../../packages/RInterop.1.0.5/lib"
 #I "packages/RProvider.1.0.5/lib"
 #I "../packages/RProvider.1.0.5/lib"
 #I "../../packages/RProvider.1.0.5/lib"
 #I "../../../packages/RProvider.1.0.5/lib"
 // Reference RProvider and RDotNet (which should be copied to the same directory)
 #r "RDotNet.dll"
+#r "RInterop.dll"
 #r "RProvider.dll"
+open RInterop
 open RProvider
 
 do fsi.AddPrinter(fun (synexpr:RDotNet.SymbolicExpression) -> synexpr.Print())
