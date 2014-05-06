3ec8ec4bf39912a140d657527f170022a34fa779
Tomas Petricek <tomas@tomasp.net>
2014-05-06 10:58:29 +0100

    Prototype of a type provider for type-safe RData access (#59)
    I wrote this prototype during a recent "type provider coding dojo" at the
    London F# user group. It needs a bit more work (some cleanup and testing),
    but it should not be too much work to finish it. I recently came across a
    [StackOverflow question][so] asking about this, so it looks like this would
    be genuinely useful.
    
     [so]: http://stackoverflow.com/questions/22991041/reading-writing-rdata-from-c-sharp-or-f-deedle-package

---
+++ src/RDataProvider/Example.fsx
@@ -0,0 +1,19 @@
+﻿#r @"..\..\bin\RDotNet.dll"
+#r @"..\..\bin\RProvider.dll"
+#r @"..\..\bin\RProvider.Runtime.dll"
+#r @"..\..\bin\RDataProvider.dll"
+
+open RProvider
+open RProvider.``base``
+
+type Foo = RData<"C:\\Temp\\Test.rdata">
+let f = new Foo()
+f.cars
+f.bar
+
+
+
+// Define your library scripting code here
+
+System.Environment.GetEnvironmentVariables()
+
---
+++ src/RDataProvider/RData.fs
@@ -0,0 +1,85 @@
+﻿namespace ProviderImplementation
+
+open Microsoft.FSharp.Core.CompilerServices
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Quotations
+open RProvider
+
+//module Globals = 
+//  let mutable initialized = false
+
+type REnv(fileName:string) =
+  let env = RInterop.callFunc "base" "new.env" [] [||] // R.new_env() 
+  do RInterop.callFunc "base" "load" (namedParams [ "file", box fileName; "envir", box env ]) [| |] |> ignore
+  member x.Environment = env
+  member x.Get(name:string) = 
+    RInterop.callFunc "base" "get" (namedParams ["x", box name; "envir", box env]) [||]
+  
+[<TypeProvider>]
+type public RDataProvider(cfg:TypeProviderConfig) as this =
+  inherit TypeProviderForNamespaces()
+
+  do 
+      System.AppDomain.CurrentDomain.add_AssemblyResolve(fun _ e ->
+        let asms = System.AppDomain.CurrentDomain.GetAssemblies()
+        let loaded = asms |> Seq.tryFind (fun asm -> asm.FullName = e.Name)
+        defaultArg loaded null )
+(*
+        // 
+
+        let name = 
+          let comma = e.Name.IndexOf(',')
+          (if comma > 0 then e.Name.Substring(0, comma) else e.Name) + ".dll"
+
+        let asmOpt = 
+          Seq.concat
+            [ cfg.ReferencedAssemblies 
+              System.IO.Directory.GetFiles(System.IO.Path.GetDirectoryName(typeof<RDataProvider>.Assembly.Location), "*.dll") ]
+          |> Seq.tryFind (fun asm -> asm.EndsWith(name, System.StringComparison.InvariantCultureIgnoreCase))
+          |> Option.map System.Reflection.Assembly.LoadFile
+
+        defaultArg asmOpt null )
+*)
+  // Boilerplate that generates root type in current assembly
+  let asm = System.Reflection.Assembly.GetExecutingAssembly()
+  let ns = "RProvider"
+  let iniType = ProvidedTypeDefinition(asm, ns, "RData", Some(typeof<obj>))
+  
+  // Add static parameter that specifies the (compile-time) ini file
+  let parameter = ProvidedStaticParameter("FileName", typeof<string>)
+  do iniType.DefineStaticParameters([parameter], fun typeName args ->
+
+    let fileName = args.[0] :?> string
+
+    let env = REnv(fileName)
+
+    // -------------------------------------------------------------
+
+    let resTy = ProvidedTypeDefinition(asm, ns, typeName, Some typeof<REnv>)
+
+    let ctor = ProvidedConstructor([])
+    ctor.InvokeCode <- fun _ -> <@@ REnv(fileName) @@>
+    resTy.AddMember(ctor)
+
+    let ctor = ProvidedConstructor([ProvidedParameter("fileName", typeof<string>)])
+    ctor.InvokeCode <- fun [fn] -> <@@ REnv(%%fn) @@>
+    resTy.AddMember(ctor)
+
+    let ls = RInterop.callFunc "base" "ls" (namedParams ["envir", box env.Environment]) [||]
+    for f in ls.GetValue<string[]>()  do
+      let v = env.Get(f)
+      try 
+        let typ = v.Value.GetType()
+        ProvidedProperty(f, typ, GetterCode = fun [self] -> Expr.Coerce(<@@ ((%%self):REnv).Get(f).Value @@>, typ))
+        |> resTy.AddMember
+      with _ -> 
+        ProvidedProperty(f, typeof<RDotNet.SymbolicExpression>, GetterCode = fun [self] ->  <@@ ((%%self):REnv).Get(f) @@>)
+        |> resTy.AddMember
+
+    resTy) 
+
+  // Register the main (parameterized) type with F# compiler
+  do this.AddNamespace(ns, [ iniType ])
+
+[<assembly:TypeProviderAssembly>]
+do()
