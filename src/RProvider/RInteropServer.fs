6d2d0f89a7289359c9f1a48c4a987820fba66e49
David Charboneau <david@adadabase.com>
2014-05-11 15:57:44 -0400

    Incorporate feedback from @tpetricek
    Removed locking from RInteropServer. Removed all references to
    C_StackSize since the RDotNet default is what is needed. Fixed
    RWrapperGenerator.fsproj so that it doesn't use elements not supported
    on Mono. Remove unnecessary reference to DesignTime assembly in Tests.

--- src/RProvider/RInteropServer.fs
+++ src/RProvider/RInteropServer.fs
@@ -10,45 +10,29 @@ open System
 type RInteropServer() =
     inherit MarshalByRefObject()
     
-    // Set the R 'R_CStackLimit' variable to -1 when initializing the R engine
-    // (the engine is initialized lazily, so the initialization always happens
-    // after the static constructor is called - by doing this in the static constructor
-    // we make sure that this is *not* set in the normal execution)
-    static do RInit.DisableStackChecking <- true
-    
     let initResultValue = RInit.initResult.Force()
-    let serverLock = "serverLock"
-    let withLock f =
-        lock serverLock f
 
     member x.RInitValue =
-        withLock <| fun () ->
-            match initResultValue with
-            | RInit.RInitError error -> Some error
-            | _ -> None
+        match initResultValue with
+        | RInit.RInitError error -> Some error
+        | _ -> None
 
     member x.GetPackages() =
-         withLock <| fun () ->
-            getPackages()
+        getPackages()
 
     member x.LoadPackage(package) =
-        withLock <| fun () ->
-            loadPackage package
+        loadPackage package
         
     member x.GetBindings(package) =
-        withLock <| fun () ->
-            getBindings package
+        getBindings package
         
     member x.GetFunctionDescriptions(package:string) =
-        withLock <| fun () ->
-            getFunctionDescriptions package
+        getFunctionDescriptions package
         
     member x.GetPackageDescription(package) =
-        withLock <| fun () ->
-            getPackageDescription package
+        getPackageDescription package
         
     member x.MakeSafeName(name) =
-        withLock <| fun () ->
-            makeSafeName name
+        makeSafeName name
 
 
