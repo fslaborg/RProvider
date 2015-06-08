a423be5fdd02ba74f3f65534e9d52109c1e6239d
J-M <jean-michel.perraud@csiro.au>
2015-06-08 19:07:25 +1000

    Update dependency to R.NET 1.6.3.

--- src/Common/AssemblyInfo.fs
+++ src/Common/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("BlueMountain Capital")>]
 [<assembly: AssemblyProductAttribute("RProvider")>]
 [<assembly: AssemblyDescriptionAttribute("An F# Type Provider providing strongly typed access to the R statistical package.")>]
-[<assembly: AssemblyVersionAttribute("1.1.8")>]
-[<assembly: AssemblyFileVersionAttribute("1.1.8")>]
+[<assembly: AssemblyVersionAttribute("1.1.9")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.9")>]
 do ()
 
