0c243060a63ccf3fb5c3c6804d595714021d3dc8
AndrewIOM <AndrewIOM@users.noreply.github.com>
2021-09-27 12:50:25 +0100

    Add some samples using local package

---
+++ samples/stan.fsx
@@ -0,0 +1,17 @@
+#i "nuget:https://www.nuget.org/api/v2"
+#i @"nuget:/Volumes/Server HD/GitHub Projects/RProvider/bin"
+#r "nuget:RProvider,2.0.0-beta"
+
+open System
+open RDotNet
+open RProvider
+open RProvider.graphics
+open RProvider.stats
+open RProvider.rstan
+open RProvider.ggplot2
+open RProvider.methods
+open RProvider.Rcpp
+open RProvider.StanHeaders
+open RProvider.rstudioapi
+
+let fit=R.stan(file= @"files/normal.stan")
